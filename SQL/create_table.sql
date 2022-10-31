ce0cafdb378989663bf30ee94ebe56f22d762085
Maxwell_Wang <maxwell.yuhuawang@outlook.com>
2022-10-31 18:33:17 +0800

    compressed data csv files

--- SQL/create_table.sql
+++ SQL/create_table.sql
@@ -1,29 +1,30 @@
-CREATE DATABASE airbnb;
-USE Airbnb;
+DROP DATABASE airbnb;
 
--- DROP DATABASE airbnb;
+CREATE DATABASE airbnb;
+ALTER DATABASE airbnb CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+USE Airbnb;
 
 CREATE TABLE `customer` (
   `cid` int PRIMARY KEY,
-  `name` varchar(40)
+  `name` nvarchar(200)
 );
 
-CREATE TABLE `neighborhood` (
-  `name` varchar(40) PRIMARY KEY,
-  `overview` varchar(500)
-);
+-- CREATE TABLE `neighborhood` (
+--   `name` varchar(200) PRIMARY KEY,
+--   `overview` varchar(2000)
+-- );
 
 CREATE TABLE `host` (
   `hid` int PRIMARY KEY,
-  `name` varchar(40),
+  `name` varchar(200),
   `since` datetime,
-  `location` varchar(40),
-  `description` varchar(500),
-  `response_time` Enum('within an hour', 'within a day', 'within a few hours'),
+  `location` varchar(1000),
+  `description` varchar(10000),
+  `response_time` Enum('within an hour', 'within a day', 'within a few hours', 'a few days or more', '') DEFAULT NULL,
   `response_rate` float,
   `acceptance` float,
   `is_super_host` int,
-  `neighborhood` varchar(40),
+  `neighborhood` varchar(200),
   `email_verified` int,
   `phone_verified` int,
   `work_email_verified` int,
@@ -32,23 +33,23 @@ CREATE TABLE `host` (
 
 CREATE TABLE `host_url` (
   `hid` int PRIMARY KEY,
-  `url` varchar(80)
+  `url` varchar(200)
 );
 
 CREATE TABLE `host_thumbnail` (
   `hid` int PRIMARY KEY,
-  `thumbnail` varchar(80)
+  `thumbnail` varchar(200)
 );
 
 CREATE TABLE `host_picture` (
   `hid` int PRIMARY KEY,
-  `picture` varchar(80)
+  `picture` varchar(200)
 );
 
 CREATE TABLE `transaction` (
   `tid` int PRIMARY KEY,
   `customer` int,
-  `property` int,
+  `property` bigint,
   `start_date` datetime,
   `end_date` datetime,
   `total_price` int,
@@ -56,49 +57,54 @@ CREATE TABLE `transaction` (
 );
 
 CREATE TABLE `listing` (
-  `lid` int PRIMARY KEY,
-  `name` varchar(40),
-  `description` varchar(500),
-  `neighborhood` varchar(40),
+  `lid` bigint PRIMARY KEY,
+  `name` varchar(500),
+  `description` varchar(10000),
+  `neighborhood` varchar(100),
+  `neighborhood_overview` varchar(2000),
   `host` int,
   `latitude` float,
   `longitude` float,
- `property_type` Enum('Entire condo', 
-       'Private room in rental unit', 
-       'Private room in condo', 
-	   'Room in hostel', 
-       'Private room in home',
-       'Entire rental unit',
-       'Entire home'),
+  `property_type` varchar(50),
+--   `property_type` Enum('Entire condo', 
+-- 	   'Room in hostel', 
+-- 	   'Room in hotel',
+--        'Private room in serviced apartment',
+--        'Private room in home',
+-- 	   'Private room in rental unit', 
+--        'Private room in condo', 
+--        'Private room in hostel',
+--        'Entire rental unit',
+--        'Entire serviced apartment',
+--        'Entire home',
+--        'Shared room in rental unit'),
   `accommodates` int,
-  `bathrooms` varchar(40),
+  `bathrooms` varchar(100),
   `bedrooms` int,
   `beds` int,
-  `amenities` varchar(500)
+  `amenities` varchar(2000)
 );
 
 CREATE TABLE `listing_url` (
-  `lid` int PRIMARY KEY,
-  `url` varchar(80)
+  `lid` bigint PRIMARY KEY,
+  `url` varchar(200)
 );
 
 CREATE TABLE `listing_picture` (
-  `lid` int PRIMARY KEY,
-  `picture` varchar(80)
+  `lid` bigint PRIMARY KEY,
+  `picture` varchar(200)
 );
 
 CREATE TABLE `calendar` (
+  `property` bigint,
   `date` datetime,
-  `property` int,
   `availability` int,
   `min_nights` int,
   `max_nights` int,
-  `price` int,
-  `adjusted_price` int,
+  `price` float,
+  `adjusted_price` float,
   PRIMARY KEY (`date`, `property`)
 );
-
-ALTER TABLE `host` ADD FOREIGN KEY (`neighborhood`) REFERENCES `neighborhood` (`name`);
 
 ALTER TABLE `host_url` ADD FOREIGN KEY (`hid`) REFERENCES `host` (`hid`);
 
@@ -110,7 +116,6 @@ ALTER TABLE `transaction` ADD FOREIGN KEY (`customer`) REFERENCES `customer` (`c
 
 ALTER TABLE `transaction` ADD FOREIGN KEY (`property`) REFERENCES `listing` (`lid`);
 
-ALTER TABLE `listing` ADD FOREIGN KEY (`neighborhood`) REFERENCES `neighborhood` (`name`);
 
 ALTER TABLE `listing` ADD FOREIGN KEY (`host`) REFERENCES `host` (`hid`);
 
